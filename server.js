d7f25e56f5972755afc02f92d952a7c5a42e548c
Eric Crescioni <ecam900@gmail.com>
2020-06-23 22:11:37 -0400

    Add simple routes

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express();
 
 // Middleware
 
-// Routes
+// Basic home route
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API.' }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in port ${PORT}...`.rainbow.bold));
