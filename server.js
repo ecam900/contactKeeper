312a9b0164edba727e10326c600c4bc7e82d013b
Eric Crescioni <ecam9@RTIAUPC0008.rti.local>
2020-06-27 23:27:05 -0400

    Change color.

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Basic home route
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper API.' }));
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the contact keeper API.' })
+);
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -19,4 +21,4 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in port ${PORT}...`.rainbow.bold));
+app.listen(PORT, () => console.log(`Server running in port ${PORT}...`.cyan));
